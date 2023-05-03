2de359325f9b48f83288529acc5cba305aeb924d
Loura <loura.collignon@ad.dfnld.nl>
2023-05-03 22:49:02 +0200

    cleaned up usings

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,9 +1,7 @@
 package Pages;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
